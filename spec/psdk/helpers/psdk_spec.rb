b1e4ac84360e3494cd40981fd40625a472c32929
Nuri Yuri <yuri@pokemonworkshop.com>
2026-01-01 18:13:00 +0100

    Check if hangup comes from exclusion in rubocop

--- spec/psdk/helpers/psdk_spec.rb
+++ spec/psdk/helpers/psdk_spec.rb
@@ -3,8 +3,8 @@
 require 'spec_helper'
 require 'psdk/helpers/psdk'
 
-RSpec.describe Psdk::Cli::PSDK do
-  describe '.unuse_local_pokemonsdk' do
+RSpec.describe Psdk::Cli::PSDK do # rubocop:disable Metrics/BlockLength
+  describe '.unuse_local_pokemonsdk' do # rubocop:disable Metrics/BlockLength
     let(:project_path) { '/path/to/project' }
     let(:psdk_path) { File.join(project_path, 'pokemonsdk') }
     let(:delete_option) { false }
@@ -35,7 +35,7 @@ RSpec.describe Psdk::Cli::PSDK do
       end
     end
 
-    context 'when psdk folder exists' do
+    context 'when psdk folder exists' do # rubocop:disable Metrics/BlockLength
       before do
         allow(Dir).to receive(:exist?).with(psdk_path).and_return(true)
         # Default non-git, non-submodule for basic checking unless overridden
@@ -43,13 +43,15 @@ RSpec.describe Psdk::Cli::PSDK do
         allow(Psdk::Cli::PSDK).to receive(:submodule?).with(project_path).and_return(false)
       end
 
-      context 'when it is not a git submodule' do
+      context 'when it is not a git submodule' do # rubocop:disable Metrics/BlockLength
         context 'with delete: true' do
           let(:delete_option) { true }
 
           it 'deletes the folder' do
             expect(FileUtils).to receive(:rm_rf).with(psdk_path)
-            expect(Psdk::Cli::PSDK).to receive(:puts).with("Successfully set project to use Pokémon Studio's PSDK version")
+            expect(Psdk::Cli::PSDK).to receive(:puts).with(
+              "Successfully set project to use Pokémon Studio's PSDK version"
+            )
 
             Psdk::Cli::PSDK.unuse_local_pokemonsdk(delete: delete_option)
           end
@@ -65,7 +67,9 @@ RSpec.describe Psdk::Cli::PSDK do
             end
 
             it 'exits with error' do
-              expect(Psdk::Cli::PSDK).to receive(:puts).with("[Error] Folder `#{new_path}` already exists. Please remove it manually.")
+              expect(Psdk::Cli::PSDK).to receive(:puts).with(
+                "[Error] Folder `#{new_path}` already exists. Please remove it manually."
+              )
               expect { Psdk::Cli::PSDK.unuse_local_pokemonsdk(delete: delete_option) }.to raise_error('Exited 1')
             end
           end
@@ -77,7 +81,9 @@ RSpec.describe Psdk::Cli::PSDK do
 
             it 'renames the folder' do
               expect(File).to receive(:rename).with(psdk_path, new_path)
-              expect(Psdk::Cli::PSDK).to receive(:puts).with("Successfully set project to use Pokémon Studio's PSDK version")
+              expect(Psdk::Cli::PSDK).to receive(:puts).with(
+                "Successfully set project to use Pokémon Studio's PSDK version"
+              )
 
               Psdk::Cli::PSDK.unuse_local_pokemonsdk(delete: delete_option)
             end
@@ -85,7 +91,7 @@ RSpec.describe Psdk::Cli::PSDK do
         end
       end
 
-      context 'when it IS a git submodule' do
+      context 'when it IS a git submodule' do # rubocop:disable Metrics/BlockLength
         before do
           allow(Psdk::Cli::PSDK).to receive(:git_project?).with(project_path).and_return(true)
           allow(Psdk::Cli::PSDK).to receive(:submodule?).with(project_path).and_return(true)
@@ -95,13 +101,15 @@ RSpec.describe Psdk::Cli::PSDK do
           let(:delete_option) { false }
 
           it 'exits with error advising to remove submodule manually' do
-            expect(Psdk::Cli::PSDK).to receive(:puts).with("[Error] Cannot use Studio's PSDK version if the project has a submodule.")
+            expect(Psdk::Cli::PSDK).to receive(:puts).with(
+              "[Error] Cannot use Studio's PSDK version if the project has a submodule."
+            )
             expect(Psdk::Cli::PSDK).to receive(:puts).with('Please follow this guide to remove the submodule: https://stackoverflow.com/a/1260982')
             expect { Psdk::Cli::PSDK.unuse_local_pokemonsdk(delete: delete_option) }.to raise_error('Exited 1')
           end
         end
 
-        context 'with delete: true' do
+        context 'with delete: true' do # rubocop:disable Metrics/BlockLength
           let(:delete_option) { true }
 
           it 'removes the submodule successfully' do
@@ -115,7 +123,9 @@ RSpec.describe Psdk::Cli::PSDK do
             expect(FileUtils).to receive(:rm_rf).with(File.join(project_path, '.git', 'modules', 'pokemonsdk'))
 
             expect(Psdk::Cli::PSDK).to receive(:puts).with('Successfully removed the submodule')
-            expect(Psdk::Cli::PSDK).to receive(:puts).with("Successfully set project to use Pokémon Studio's PSDK version")
+            expect(Psdk::Cli::PSDK).to receive(:puts).with(
+              "Successfully set project to use Pokémon Studio's PSDK version"
+            )
 
             Psdk::Cli::PSDK.unuse_local_pokemonsdk(delete: delete_option)
           end
@@ -123,7 +133,9 @@ RSpec.describe Psdk::Cli::PSDK do
           it 'fails to deinit submodule' do
             expect(Psdk::Cli::PSDK).to receive(:system).with('git', 'submodule', 'deinit', any_args).and_return(false)
 
-            expect(Psdk::Cli::PSDK).to receive(:puts).with('[Error] Failed to remove the submodule (Failed to deinit pokemonsdk submodule)')
+            expect(Psdk::Cli::PSDK).to receive(:puts).with(
+              '[Error] Failed to remove the submodule (Failed to deinit pokemonsdk submodule)'
+            )
             expect { Psdk::Cli::PSDK.unuse_local_pokemonsdk(delete: delete_option) }.to raise_error('Exited 1')
           end
 
@@ -131,7 +143,9 @@ RSpec.describe Psdk::Cli::PSDK do
             expect(Psdk::Cli::PSDK).to receive(:system).with('git', 'submodule', 'deinit', any_args).and_return(true)
             expect(Psdk::Cli::PSDK).to receive(:system).with('git', 'rm', any_args).and_return(false)
 
-            expect(Psdk::Cli::PSDK).to receive(:puts).with('[Error] Failed to remove the submodule (Failed to remove pokemonsdk submodule)')
+            expect(Psdk::Cli::PSDK).to receive(:puts).with(
+              '[Error] Failed to remove the submodule (Failed to remove pokemonsdk submodule)'
+            )
             expect { Psdk::Cli::PSDK.unuse_local_pokemonsdk(delete: delete_option) }.to raise_error('Exited 1')
           end
         end

f34332a0900aac4412f184a1363a9d41cb738f24
Nuri Yuri <yuri@pokemonworkshop.com>
2026-04-26 11:43:49 +0200

    Fix spec issues

--- spec/psdk/cli/plugin_spec.rb
+++ spec/psdk/cli/plugin_spec.rb
@@ -37,12 +37,15 @@ RSpec.describe Psdk::Cli::Plugin do # rubocop:disable Metrics/BlockLength
   describe '#build' do # rubocop:disable Metrics/BlockLength
     before do
       allow(Psdk::Cli::Configuration).to receive(:get).with(:local)
-      allow(Psdk::Cli::Configuration).to receive(:project_path).and_return('/path/to/project')
       allow(Psdk::Helpers::PluginManager).to receive(:build)
       subject.options = { out_dir: '.' }
     end
 
     context 'when in a PSDK project' do
+      before do
+        allow(Psdk::Cli::Configuration).to receive(:project_path).and_return('/path/to/project')
+      end
+
       it 'calls Psdk::Helpers::PluginManager.build with plugin name' do
         expect(Psdk::Helpers::PluginManager).to(
           receive(:build).with('my_plugin', in_project: '/path/to/project', out_dir: '.')
@@ -57,7 +60,9 @@ RSpec.describe Psdk::Cli::Plugin do # rubocop:disable Metrics/BlockLength
     end
 
     context 'when in standalone mode' do
-      let(:config_mock) { double('Configuration', project_path: nil) }
+      before do
+        allow(Psdk::Cli::Configuration).to receive(:project_path).and_return(nil)
+      end
 
       it 'calls Psdk::Helpers::PluginManager.build with default name' do
         expect(Psdk::Helpers::PluginManager).to receive(:build).with('.', in_project: nil, out_dir: '.')
